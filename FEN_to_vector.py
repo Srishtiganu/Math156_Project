786993f3d9094ba2466db7f5b6c8c8501ca56e64
PoweredWarrior1729 <henry#lindhurst.com>
2024-12-04 17:05:11 -0800

    Added comments to the utility files

--- FEN_to_vector.py
+++ FEN_to_vector.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+# Dictionaries to convert pieces and letters to numbers
 piece_to_num = {'p':0, 'P':1, 'r':2, 'R':3, 'n':4, 'N':5, 'b':6, 'B':7, 'q':8, 'Q':9, 'k':10, 'K':11}
-
 alphabet = {'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7}
 
+# Convert a FEN representation of the board into a vector of numbers
 def to_vector(FEN):
+    # Split the FEN string into different parts to process separately
     parts = FEN.split()
     board = parts[0]
     board_vector = np.zeros((8,8,12))
@@ -14,9 +16,13 @@ def to_vector(FEN):
     castling_vector = np.array([0,0,0,0])
     ep = parts[3]
     ep_vector = np.zeros((8,2))
+    # This tracks how many moves until the 50 move rule forces a draw
     half_move_vector = np.array([int(parts[4])])
     i = 0
     j = 0
+    
+    # Loop through the board representation, setting values appropriately
+    # if there is a piece at the corresponding square
     for char in board:
         if char.isalpha():
             board_vector[i][j][piece_to_num[char]] = 1
@@ -27,11 +33,13 @@ def to_vector(FEN):
             j += 1
             i = 0
 
+    # Set a parameter based on whether white or black is to move
     if player == 'w':
         player_vector[0] = 0
     if player == 'b':
         player_vector[0] = 1
 
+    # Set a castling rights vector
     if 'K' in castling:
         castling_vector[0] = 1
     if 'Q' in castling:
@@ -41,11 +49,13 @@ def to_vector(FEN):
     if 'q' in castling:
         castling_vector[3] = 1
     
+    # Set a flag if en passant is legal and denote where
     if ep != '-':
         file = alphabet[ep[0]]
         row = int(int(ep[1]) / 3 - 1)
         ep_vector[file][row] = 1
     
+    # Append all of the data vectors and return the result
     v1 = board_vector.flatten()
     v2 = player_vector
     v3 = castling_vector
--- chess_harness.py
+++ chess_harness.py
@@ -2,6 +2,9 @@ import random
 import chess
 import os
 
+# Get a move from the user that indicates their move
+# Moves are denoted by a source and destination square
+# If the move is invalid it asks again. The player can also resign by typing "resign"
 def get_move(board):
     legal_moves = board.legal_moves
     move = input("Enter move: ")
@@ -26,6 +29,7 @@ def get_move(board):
             except:
                 pass
 
+# If the player did not resign, update the board with the move they input, otherwise resign
 def process_player_move(board):
     player_move = get_move(board)
     try:
@@ -36,13 +40,16 @@ def process_player_move(board):
         print(board)
         return 1
 
+# Allow the network to make the best move according to its evaluation function
 def process_computer_move(board, eval, player):
     best_eval = None
     best_move = None
+    # Initialize the best move variable with the worst possible case
     if player == 'white':
         best_eval = float("inf")
     if player == 'black':
         best_eval = float("-inf")
+    # Iterate through the legal moves, evaluate them, and keep track of the best one
     for move in board.legal_moves:
         board.push(move)
         value = eval_harness(board, eval)
@@ -55,12 +62,14 @@ def process_computer_move(board, eval, player):
             if value > best_eval:
                 best_eval = value
                 best_move = move
+    # Make the best move
     board.push(best_move)
     print(board)
 
-
+# Play a Chess game
 def game(eval_function, player='select'):
-    while True:
+    # Decide on whether the player will play as white or black
+    while player not in ['white', 'black']:
         player = input("Enter whether you will play as white or black: ")
         if player in ['white', 'w', 'White', 'W']:
             player = 'white'
@@ -68,18 +77,22 @@ def game(eval_function, player='select'):
         if player in ['black', 'b', "Black", 'B']:
             player = 'black'
             break
+    # Initialize the board. If the player goes second, the computer makes the first move
     board = chess.Board()
     print(board)
     if player == 'black':
         process_computer_move(board, eval_function, player)
+    # Process player and computer moves until the game ends
     while True:
         rc = process_player_move(board)
+        # Detect resgination and respond accordingly
         if rc == 1:
             if player == 'black':
                 print('1-0')
             if player == 'white':
                 print('0-1')
             return
+        # Process moves other than resgnation
         if board.outcome() is not None:
             print(board.result())
             return
@@ -88,9 +101,11 @@ def game(eval_function, player='select'):
             print(board.result())
             return
 
+# Return a random number as the board evaluation
 def random_eval(board):
     return random.randint(-1000,1000)
 
+# Evaluate positins where the game is over with their true values. Otherwise call the evaluation function
 def eval_harness(board, eval=random_eval):
     if board.outcome() is not None:
         res = board.result()
